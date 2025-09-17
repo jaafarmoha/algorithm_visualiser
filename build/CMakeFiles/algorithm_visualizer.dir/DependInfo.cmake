b75f88c541631613a0e010737d79c61284b52788
Jaafar Mohammed <jaaf2006@icloud.com>
2025-09-17 19:07:06 +0100

    CMake built and window initialised

---
+++ build/CMakeFiles/algorithm_visualizer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jaafarmohammed/Desktop/Personal Projects/algorithm_visualiser/src/glad.c" "CMakeFiles/algorithm_visualizer.dir/src/glad.c.o" "gcc" "CMakeFiles/algorithm_visualizer.dir/src/glad.c.o.d"
+  "/Users/jaafarmohammed/Desktop/Personal Projects/algorithm_visualiser/src/main.cpp" "CMakeFiles/algorithm_visualizer.dir/src/main.cpp.o" "gcc" "CMakeFiles/algorithm_visualizer.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/algorithm_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm_visualizer.dir/src/glad.c.o"
+  "CMakeFiles/algorithm_visualizer.dir/src/glad.c.o.d"
+  "CMakeFiles/algorithm_visualizer.dir/src/main.cpp.o"
+  "CMakeFiles/algorithm_visualizer.dir/src/main.cpp.o.d"
+  "algorithm_visualizer"
+  "algorithm_visualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/algorithm_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
